9bfd3d29b6791b13853424aea6b1543cb774a1be
MacBook Pro <macbookpro@Ziyods-MacBook-Pro.local>
2024-07-29 02:35:12 +0500

    fixed

--- app/product/page.jsx
+++ app/product/page.jsx
@@ -7,7 +7,7 @@ import HomePageNavButton from '@/components/UI/HomePageNavButton';
 import Image from 'next/image';
 
 const index = () => {
-    const router = useRouter();
+  const router = useRouter()
 
   const info = [
     { image: AksiyaSportivka },
